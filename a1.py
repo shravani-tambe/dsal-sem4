1e88d2f4e1469e69effcef802450e63290e09128
Shravani Tambe <154653607+shravani-tambe@users.noreply.github.com>
2025-05-07 05:56:27 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,64 +1,69 @@
-def tele_database():
-    phone_data = []
-    n = int(input("Enter Number of Clients :- "))
-    print("Enter Phone Numbers --\n")
-    for _ in range(n):
-        x = int(input("--> "))
-        phone_data.append(x)
-    return phone_data
+def teledatabase(): 
+    phone = [] 
+    n = int(input("Enter number of clients: "))
+    for i in range(n): 
+        x = int(input(f"Enter phone number {i+1}: "))
+        phone.append(x)
+    return phone 
+    
+def hash1(key, table_size): 
+    return key % table_size
+    
+def hash2(key): 
+    return 7 - (key % 7)
+    
+def display_table(hashtable): 
+    print(f"\nHash Index\tPhone Number")
+    for i in range(len(hashtable)): 
+        if hashtable[i] != -1: 
+            print(f"{i}\t\t{hashtable[i]}")
+        else: 
+            print(f"{i}\t\tEMPTY")
+                
+while True: 
+    phone = teledatabase() 
+    m = int(input("Enter Hash Table Size: "))
+    hashtable = [-1] * m
+    
+    opt = int(input(
+        "\nChoose collision resolution technique:\n"
+        "1. Linear Probing\n"
+        "2. Double Hashing\n"
+        "3. Exit\n"
+        "Choice: "
+    ))
+    
+    if opt == 3:
+        print("TERMINATED SUCCESSFULLY")
+        break
+    
+    for num in phone: 
+        h1 = hash1(num, m)
+        
+        if hashtable[h1] == -1: 
+            hashtable[h1] = num
+        else: 
+            if opt == 1:  # Linear Probing
+                orig_h1 = h1
+                while hashtable[h1] != -1:
+                    h1 = (h1 + 1) % m
+                    if h1 == orig_h1:
+                        print("Hash Table is full!")
+                        break
+                else:
+                    hashtable[h1] = num
 
-
-def hash_function_1(key_ele, m_size):
-    h1 = key_ele % m_size
-    return h1
-
-
-def hash_function_2(key_ele):
-    h2 = 7 - (key_ele % 7)
-    return h2
-
-
-def hashtable(ht):
-    print(f"\nHash Value \tKey")
-    for ele in range(len(ht)):
-        if ht[ele] != -1:
-            print(f"\n\t{ele} \t --> \t{ht[ele]}")
-        else:
-            print(f"\n\t{ele}")
-
-#main 
-
-while (1):
-    phone_database = tele_database()
-    m = int(input("Enter Hash Table Size :- "))
-    hash_table = [-1] * m #creates a list with size m, all elements as -1
-    opt = int(input("If collision occurs which collision resolution technique do you want to use?\n\t1. Linear Probing\n\t2. Double Hashing\n\t3. Exit :- "))
-    for k in phone_database:
-        h_1 = hash_function_1(k, m)
-        h_2 = hash_function_2(k)
-
-        if hash_table[h_1] == -1:
-            hash_table[h_1] = k
-        else:
-            if opt == 1:
-                while hash_table[h_1] != -1:
-                    h_1 += 1
-                hash_table[h_1] = k
-                hashtable(hash_table)
-
-            elif opt == 2:
-                i = 0
-                while hash_table[h_1] != -1:
+            elif opt == 2:  # Double Hashing
+                h2 = hash2(num)
+                i = 1
+                orig_h1 = h1
+                while hashtable[h1] != -1:
+                    h1 = (orig_h1 + i * h2) % m
                     i += 1
-                    h_1 = (h_1 + (i * h_2)) % m
-                hash_table[h_1] = k
-                hashtable(hash_table)
+                    if i > m:
+                        print("Hash Table is full!")
+                        break
+                else:
+                    hashtable[h1] = num
 
-            elif opt == 3:
-                print("\n*** TERMINATED SUCCESSFULLY ***")
-                exit(0)
-
-
-
-
-###later implement quadratic probing in this only
+    display_table(hashtable)
