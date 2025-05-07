5712c48423c731ceb8d3137b1bd94c7cbc239bed
Shravani Tambe <154653607+shravani-tambe@users.noreply.github.com>
2025-05-07 06:11:32 +0530

    Update c21.java

--- c21.java
+++ c21.java
@@ -1,45 +1,42 @@
 import java.util.Arrays;
-import java.util.*;
-public class Sort
-{
-    public static void shell(int[] a)
-    {
-        int increment=a.length/2;
-        while(increment>0)
-        {
-            for(int i=increment;i<a.length;i++)
-            {
-                int j=i;
-                int temp=a[i];
-                while(j>=increment &&a[j-increment]>temp)
-                {
-                    a[j]=a[j-increment];
-                    j=j-increment;
+import java.util.Scanner;
+
+public class Sort {
+
+    public static void shell(int[] a) {
+        int increment = a.length / 2;
+        while (increment > 0) {
+            for (int i = increment; i < a.length; i++) {
+                int temp = a[i];
+                int j = i;
+                while (j >= increment && a[j - increment] > temp) {
+                    a[j] = a[j - increment];
+                    j -= increment;
                 }
-                a[j]=temp;
+                a[j] = temp;
             }
-            if(increment==2)
-                increment=1;
+            if (increment == 2)
+                increment = 1;
             else
-                increment*=(5.0/11);
+                increment *= 5.0 / 11;
         }
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int SIZE = 10;
+        int[] n = new int[SIZE];
 
-    public static void main(String[] args)
-    {
-        Sort ob= new Sort();
-        int n[]=new int[10];
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter 10 Elements:");
-        for(int i=0;i<10;i++)
-        {
-            n[i]=sc.nextInt();
+        System.out.println("Enter " + SIZE + " Elements:");
+        for (int i = 0; i < SIZE; i++) {
+            n[i] = sc.nextInt();
         }
-        //int nums[]= {7,-5,3,2,1,0,45};
+
         System.out.println("Original Array:");
         System.out.println(Arrays.toString(n));
-        ob.shell(n);
+
+        Sort.shell(n);
+
         System.out.println("Sorted Array:");
         System.out.println(Arrays.toString(n));
     }
